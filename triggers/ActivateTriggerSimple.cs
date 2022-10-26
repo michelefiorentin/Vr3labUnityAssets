7ef8bb15d3c50ca639ec5b7486fe59474c934672
Mike Flower <michele.fiorentino@poliba.it>
2022-10-26 18:39:59 +0200

    improved trigger ( to be tested)

---
+++ triggers/ActivateTriggerSimple.cs
@@ -0,0 +1,34 @@
+/// This simple script triggers a custom event 
+/// the Gameobject requires a collider with is triggered enabled 
+/// By Michele Fiorentino
+/// Politecnico di bari
+/// Copyright 2022
+/// 
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class ActivateTriggerSimple : MonoBehaviour
+{
+    // A multi-purpose script which causes an action to occur when
+    // a trigger collider is entered.
+    [Tooltip("Put here events to be triggered")]
+    public UnityEvent yourCustomEvent; // the events to be run
+    [Tooltip("Put here scenes to be loaded")]
+    public string SceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (yourCustomEvent != null)
+        {
+            yourCustomEvent.Invoke();
+            //Debug.Log("custome event invoked");
+        }
+        if (!string.Empty(SceneName))
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+    }
+}
+
