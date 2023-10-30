9f6be438ebe530ed621fdc7f1382e59cf24b99df
Mike Flower <michele.fiorentino@poliba.it>
2023-10-30 10:13:33 +0100

    add load

--- scripts 2023/sceneloader/LoadScene.cs
+++ scripts 2023/sceneloader/LoadScene.cs
@@ -1,46 +1,50 @@
 /*
-IMPORTANT NOTE: uses legacy input system
-Copyright Michele Fiorentino 2023 - Politecnico di Bari
+IMPORTANT WARNING: uses legacy input system
+Copyright Michele Fiorentino 2021 - 23 Politecnico di Bari
 Brief: This is a signle script for avoiding student to program.
 Enjoy! MIT license
+30-10-23 improved 
 Usage: 
-1) create a empty object
+1) create target scene give an easyname (no spaces)
 2) add target scene to Build settings
-3) set key and the name of the scene (avoid spaces)
-4) enjoy
+3) set the name of the scene (with spaces)
+4) call public loadscene or debug key (using legacy input - may not work on new input system)  
 */
 using System;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-namespace UnityStandardAssets.Utility
-{
     public class LoadScene : MonoBehaviour
     {
-        [Tooltip("Set here the Name of the Scene (no .unity extension needed)- scene must be added to the build scenes")]
-        public bool startAtactivation  = true; // the events to be run
- 
+        [Tooltip("Set here the key name- default is a")]
+        public string keyname ="s";
         [Tooltip("Set here the Name of the Scene (no .unity extension needed)- scene must be added to the build scenes")]
         public string scene_to_be_loaded =""; // the events to be run
 
-        [Tooltip("This public script can be calle dal so from Custom event")]
-        public void loadscene()
+        private void Update()
         {
-            if (!String.IsNullOrEmpty(scene_to_be_loaded))
+            if (Input.GetKeyDown(keyname))
             {
-                print("load" + scene_to_be_loaded);
-                SceneManager.LoadScene(scene_to_be_loaded);
+            Debug.Log("Fired key: " + keyname);
+            loadScene();
+
             }
-
         }
 
-        private void Start()
+    public void loadScene(string p_scene)
+    {
+
+        if (!String.IsNullOrEmpty(p_scene))
         {
-            if (startAtactivation) loadscene();
-       
-            //loadscene();
+            Debug.Log("Loading: " + p_scene);
+            SceneManager.LoadScene(p_scene);
         }
+    }
 
+    public void loadScene()
+    {
+        loadScene(scene_to_be_loaded);
     }
 }
+
